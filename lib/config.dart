450d4ddaa90317933c60ff1913cb2b7e1be24f3f
BlessForever04 <vinitwankhade411@gmail.com>
2026-02-14 16:36:14 +0530

    Config file fixed, base url working for android env as well as chrome

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,14 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class Config {
-  static const String baseUrl = 'http://localhost:8080';
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:8080';
+    } else if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    } else {
+      return 'http://localhost:8080';
+    }
+  }
 }

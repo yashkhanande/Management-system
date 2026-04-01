2cbda77662ba912386dbf2ff0e20382fdf2c2dc1
BlessForever04 <vinitwankhade411@gmail.com>
2026-04-01 15:32:58 +0530

    Pushed the changed api

--- lib/config.dart
+++ lib/config.dart
@@ -5,18 +5,10 @@ class Config {
   static const String _apiBaseUrl = String.fromEnvironment('API_BASE_URL');
 
   static String get baseUrl {
-
     if (_apiBaseUrl.isNotEmpty) {
       return _apiBaseUrl;
     }
 
-    if (kIsWeb) {
-      return 'http://localhost:8080';
-    } else if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8080';
-    } else {
-      return 'http://localhost:8080';
-    }
+    return 'https://management-app-backend-kyhx.onrender.com';
   }
 }
-

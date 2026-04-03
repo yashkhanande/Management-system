816b1ce5400a0aee65285f7bf28bc11528c43c3a
Yash Khanande <yashkhanande7@gmail.com>
2026-04-03 23:31:51 +0530

    project task dependency

--- lib/config.dart
+++ lib/config.dart
@@ -8,8 +8,8 @@ class Config {
     if (_apiBaseUrl.isNotEmpty) {
       return _apiBaseUrl;
     }
-
-    return 'http://10.0.2.2:8080';
+    return 'http://localhost:8080';
+    // return 'http://10.0.2.2:8080';
     // return 'https://management-app-backend-kyhx.onrender.com';
   }
 }
